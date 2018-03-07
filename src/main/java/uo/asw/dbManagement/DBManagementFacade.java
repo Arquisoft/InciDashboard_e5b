30e4279747e3831886b62560e7679c893c3cb020
adrycarou896 <adripc@live.com>
2018-03-08 00:34:12 +0100

    Coment consultas en DBMAnagementAFacde porque creo que van en otro sitio

--- src/main/java/uo/asw/dbManagement/DBManagementFacade.java
+++ src/main/java/uo/asw/dbManagement/DBManagementFacade.java
@@ -9,20 +9,19 @@ import uo.asw.dbManagement.model.Incidence;
 
 public interface DBManagementFacade {
 	
-	@Query("SELECT f FROM Filter f WHERE f.id = ?1")
+	//@Query("SELECT f FROM Filter f WHERE f.id = ?1")
 	public Filter getFilter(long idFilter);
 
 	public void updateFilter(Filter Filter);	
 	
-	@Query("SELECT i FROM Incidence i WHERE i.id = ?1")
+	//@Query("SELECT i FROM Incidence i WHERE i.id = ?1")
 	public Incidence getIncidence(long idIncidence);
 	
-	@Query("SELECT o.incidences FROM Operator o WHERE o.id = ?1")
+	//@Query("SELECT o.incidences FROM Operator o WHERE o.id = ?1")
 	public List<Incidence> getOperatorIncidences(long idOperator); //TODO - Cuando este implementado y funcione, hacer que devuelva Page<Incidence> para meter paginacion
 
 	public List<Incidence> getIncidencesOfCategory(List<String> categories); //TODO - Cuando este implementado y funcione, hacer que devuelva Page<Incidence> para meter paginacion
 	
-	//@Query("UPDATE Incidence SET incidence = ?2 WHERE id = ?1")
 	public void updateIncidence(Incidence incidence);//¿?¿?¿
 	
 }
