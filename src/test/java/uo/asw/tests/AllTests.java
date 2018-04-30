17bfbab19e9d2b98e1acf0a7a77f912313feb627
adrycarou896 <adripc@live.com>
2018-04-30 19:04:07 +0200

    Mejora

--- src/test/java/uo/asw/tests/AllTests.java
+++ src/test/java/uo/asw/tests/AllTests.java
@@ -4,16 +4,9 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-import uo.asw.tests.business.FilterTest;
-import uo.asw.tests.business.RIncidencePTest;
-import uo.asw.tests.cucumber.CucumberTest;
-
 @RunWith(Suite.class)
 @SuiteClasses({
 	
-	RIncidencePTest.class
-	,FilterTest.class,
-	CucumberTest.class
 	
 })
 public class AllTests { }
